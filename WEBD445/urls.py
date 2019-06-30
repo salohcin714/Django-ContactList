5592345270a88ba81e5b87a71ff6230c86eb5175
Nicholas Norris <41271531+salohcin714@users.noreply.github.com>
2019-06-30 13:18:31 -0400

    Update urls.py

--- WEBD445/urls.py
+++ WEBD445/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contacts/', include('contacts.urls'))
+    path('', include('contacts.urls'))
 ]
