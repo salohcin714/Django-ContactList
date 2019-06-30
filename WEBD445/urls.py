b173e80c72df243303454596ee07d167469a7337
Nicholas Norris <41271531+salohcin714@users.noreply.github.com>
2019-06-30 13:24:33 -0400

    Redirect fix

--- WEBD445/urls.py
+++ WEBD445/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import redirect_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contacts.urls'))
+    path('contacts/', include('contacts.urls'))
+    path('', redirect_view)
 ]
